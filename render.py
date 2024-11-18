8aee60a733350e25f700c87305cbc12ea1c6fd55
Tucker Beck <tucker.beck@gmail.com>
2024-11-18 14:51:12 -0800

    Added some sizzle

--- render.py
+++ render.py
@@ -10,18 +10,21 @@ from weasyprint import HTML
 app = typer.Typer(rich_markup_mode="rich")
 
 
+class ColorScheme(AutoNameEnum):
+    light = auto()
+    night = auto()
+    sizzle = auto()
+
+
 @app.command()
-def render(dark: Annotated[bool, typer.Option(help="Render in dark mode.")] = False):
+def render(
+    color: Annotated[ColorScheme, typer.Option(help="Render with color scheme.")] = ColorScheme.light,
+    output: Annotated[Path, typer.Option(help="The cv file to produce.")] = Path("tucker-beck-cv.pdf"),
+):
     md_path = Path("README.md")
     html_content = markdown(md_path.read_text())
-    css_paths = [Path("etc/styles.css")]
-
-    if dark:
-        css_paths.append(Path("etc/dark.css"))
-    else:
-        css_paths.append(Path("etc/light.css"))
-
-    HTML(string=html_content).write_pdf("tucker-beck-cv.pdf", stylesheets=css_paths)
+    css_paths = [Path("etc/styles.css"), Path(f"etc/{color}.css")]
+    HTML(string=html_content).write_pdf(output, stylesheets=css_paths)
 
 
 if __name__ == "__main__":
